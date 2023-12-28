79da7e854785abecc4e61c0720f28d52ac19e5bc
Nathan Le Fütür Milliønãîre <misterntkofficiel2.0@gmail.com>
2023-12-28 09:52:25 +0100

    Le routeur

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <?php
 
+// Includes all the controllers files
+
+use Ysthote\Controllers\Viewing\Views;
+
+require_once('src/controllers/Views.php');
+
 // This is the rooter, that will call the right controller
 if(isset($_GET['page']) && $_GET['page'] !== '') {
-
+    if($_GET['page'] === 'home') {
+        $homeView = new Views();
+        $homeView->homePage();
+    } elseif($_GET['page'] === 'authentication') {
+        $enroll = new Views();
+        $enroll->enroll();
+    }
 } else {
-    
+    $homeView = new Views();
+    $homeView->homePage();
 }
