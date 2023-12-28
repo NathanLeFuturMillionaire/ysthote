766bb91974a9d937bd05c7dd113a31b8ca1bf8da
Nathan Le Fütür Milliønãîre <misterntkofficiel2.0@gmail.com>
2023-12-28 09:53:36 +0100

    Traite les données isssues du formulaire d'inscription

---
+++ src/controllers/auth/Enroll.php
@@ -0,0 +1,13 @@
+<?php
+
+// Init a namespace
+namespace Ysthote\Controllers\Auth;
+
+require_once('src/libs/database.php');
+
+use Ysthote\Libs\Database\DatabaseConnection;
+
+class Enroll
+{
+    
+}
