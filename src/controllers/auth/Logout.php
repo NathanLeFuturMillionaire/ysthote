9baf728e0b3a61289b5c1571519d4745480da27e
Nathan Le Fütür Milliønãîre <misterntkofficiel2.0@gmail.com>
2024-01-30 09:43:08 +0100

    My commit

---
+++ src/controllers/auth/Logout.php
@@ -0,0 +1,14 @@
+<?php
+
+class Logout
+{
+    public function logout()
+    {
+        // Vérifie si une session existe déjà
+        if(isset($_SESSION['ID'])) {
+            // Mets fin à la session et redirige vers la page de connexion
+            session_destroy();
+            header('Location: index.php?page=login');
+        }
+    }
+}
--- templates/error.php
+++ templates/error.php
@@ -1,10 +1,20 @@
 <?php $title = 'Ysthote - Une erreur s\'est produite'; ?>
 
-<?php ob_start(); ?>
+<?php
+
+ob_start();
+
+?>
 
 <main class="error-message">
     <header>Une erreur s'est produite</header>
     <p><?= $errorMessage; ?></p>
+
+    <?php
+        if(isset($_SESSION['ID'])) {
+            echo '<a href="index.php?page=logout">Se déconnecter</a>';
+        }
+    ?>
 </main>
 
 <?php $content = ob_get_clean(); ?>
