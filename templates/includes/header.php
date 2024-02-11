5be9426abcbee756d948d4ffef07cf00e08a604a
Nathan Le Fütür Milliønãîre <misterntkofficiel2.0@gmail.com>
2024-02-12 00:38:24 +0100

    La vue du header

--- templates/includes/header.php
+++ templates/includes/header.php
@@ -1,10 +1,27 @@
 <header class="included-header">
     <div class="logo">
-        <h1><a href="index.php">Ysthote</a></h1>
+        <?php // If a session is opened, show log out links and other
+        if (isset($_SESSION['ID']) || isset($_SESSION['EMAIL'])) {
+        ?>
+            <h1><a href="index.php?page=profil">Ysthote</a></h1>
+        <?php
+        } else {
+        ?>
+            <h1><a href="index.php">Ysthote</a></h1>
+        <?php
+        }
+        ?>
+
     </div>
     <nav class="included-navbar">
         <ul><?php // If a session is opened, show log out links and other
             if (isset($_SESSION['ID']) || isset($_SESSION['EMAIL'])) {
+                ?>
+                <form action="" method="post">
+                    <!-- <input type="search" name="search" id="search">
+                    <input type="submit" value="Rechercher"> -->
+                </form>
+                <?php
             } else {
             ?>
                 <li><a href="index.php?page=authentication">S'inscrire</a></li>
